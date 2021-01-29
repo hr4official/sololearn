5afbd57b78a9d47daf6ef8243fe56d980e346765
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-01-29 08:45:37 +0530

    update

--- solo/appalone/admin.py
+++ solo/appalone/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-
+#registering for reflecting in admin panel
 admin.site.register(User)
--- solo/appalone/models.py
+++ solo/appalone/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#database 
 class User(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
