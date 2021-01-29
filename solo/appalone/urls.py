cf21c5db46a04a3bc8323a6eb29b4cb05e5818d6
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-01-29 08:54:45 +0530

    signup and login page added

--- solo/appalone/urls.py
+++ solo/appalone/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.homepage,name="homepage"),
+    path("signup",views.signup,name="signup"),
+    path("login",views.login,name="login"),
 ]
--- solo/appalone/views.py
+++ solo/appalone/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request,"homepage.html")
+
+def signup(request):
+    return render(request,"signup.html")
+
+def login(request):
+    return render(request,"login.html")
